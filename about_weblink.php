c3447d8afc773628cfec969b3ff602ca37aba0d8
atanu sahoo <atanu.sahoo@sbinfowaves.com>
2026-06-09 15:46:15 +0530

    update new code test

---
+++ about_weblink.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ABOUT</title>
+<style>
+.about_img{
+ width:95%;
+ margin:0 auto;
+ display:block;
+}
+</style>
+</head>
+
+<body>
+<h2 style="text-align:center;margin-top:20px;"></h2>
+<p style="text-align:justify;">
+Star Cement Limited is the leading cement manufacturer in North East India
+and is the No. 1 cement brand in the North East. Set up in 2004, the plant
+is spread across 200 hectares of land & built with state of the art German
+Technology in the idyllic town of Lumshnong, a strategic location at
+Meghalaya, a place that ensures easy availability of high-grade limestone.
+Star Cement Plant is the largest in North East & one of the most advanced in
+India.
+<br /><br />
+Star Cement has also fast expanding its footprint in West Bengal & Bihar.
+With a distribution network with over 6000 plus in North East India and 6000
+Plus in Bengal and Bihar, Star Cement produces three grades of cement namely
+PPC, PSC and Anti rust Cement known & admired for its high quality, high
+strength cement.
+</p>
+<br />
+<img src="about_star_stellar.jpg" class="about_img" />
+</body>
+</html>
